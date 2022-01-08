77dea5b0b7269acde23037c66ac81c917e5c2931
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-01-08 14:34:19 -0300

    chore: styles global

---
+++ src/styles/global.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+  #root, body, html {
+    -webkit-font-smoothing: antialiased;
+  
+  }
+
+  ${({ theme }) => css`
+
+    html {
+        font-size: 10px;
+        background: ${theme.colors.white};
+    }
+
+    body, input, button {
+      font-family: ${theme.font.family};
+    }
+    
+  `}
+ 
+  button {
+    cursor: pointer;
+  }
+`;
