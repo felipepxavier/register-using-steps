e25e24c9acd80069367f782ec96a1055dd7330be
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-01-09 14:26:16 -0300

    test: should not give an error when clicking 'Continuar' if you dont have a next step

--- src/components/StepWizard/step-wizard.spec.tsx
+++ src/components/StepWizard/step-wizard.spec.tsx
@@ -190,4 +190,148 @@ describe('<StepWizard />', () => {
 
     expect(screen.queryByLabelText(/cep/i)).not.toBeInTheDocument();
   });
+
+  it('should not give an error when clicking "Voltar" if you dont have a previous step', async () => {
+    const stepsField = [
+      {
+        active: true,
+
+        fields: [
+          {
+            name: 'name',
+            label: 'Nome',
+            required: true,
+          },
+          {
+            name: 'last-name',
+            label: 'Sobrenome',
+            required: true,
+          },
+          {
+            name: 'email',
+            label: 'E-mail',
+            required: true,
+          },
+          {
+            name: 'phone',
+            label: 'Telefone',
+            required: false,
+          },
+        ],
+      },
+      {
+        active: false,
+        fields: [
+          {
+            name: 'zip-code',
+            label: 'CEP',
+            required: true,
+          },
+          {
+            name: 'address-1',
+            label: 'Endereço 1',
+            required: true,
+          },
+          {
+            name: 'address-2',
+            label: 'Endereço 2',
+            required: false,
+          },
+        ],
+      },
+    ];
+    renderWithTheme(<StepWizard totalSteps={stepsField} />);
+
+    await act(async () => {
+      const btnBack = screen.getByText('Voltar');
+      fireEvent.click(btnBack);
+    });
+
+    expect(screen.queryByLabelText(/cep/i)).not.toBeInTheDocument();
+    expect(screen.getByLabelText(/^nome/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/sobrenome/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/telefone/i)).toBeInTheDocument();
+  });
+
+  it('should not give an error when clicking "Continuar" if you dont have a next step', async () => {
+    const stepsField = [
+      {
+        active: true,
+
+        fields: [
+          {
+            name: 'name',
+            label: 'Nome',
+            required: true,
+          },
+          {
+            name: 'last-name',
+            label: 'Sobrenome',
+            required: true,
+          },
+          {
+            name: 'email',
+            label: 'E-mail',
+            required: true,
+          },
+          {
+            name: 'phone',
+            label: 'Telefone',
+            required: false,
+          },
+        ],
+      },
+      {
+        active: false,
+        fields: [
+          {
+            name: 'zip-code',
+            label: 'CEP',
+            required: true,
+          },
+          {
+            name: 'address-1',
+            label: 'Endereço 1',
+            required: true,
+          },
+          {
+            name: 'address-2',
+            label: 'Endereço 2',
+            required: false,
+          },
+        ],
+      },
+    ];
+    renderWithTheme(<StepWizard totalSteps={stepsField} />);
+
+    const nameInput = screen.getByPlaceholderText(/^nome/i);
+    const lasNameInput = screen.getByPlaceholderText(/sobrenome/i);
+    const emailInput = screen.getByPlaceholderText(/e-mail/i);
+
+    fireEvent.change(nameInput, { target: { value: 'myName' } });
+    fireEvent.change(lasNameInput, { target: { value: 'lastName' } });
+    fireEvent.change(emailInput, { target: { value: 'email' } });
+
+    const btnNext = screen.getByText('Continuar');
+
+    await act(async () => {
+      fireEvent.click(btnNext);
+    });
+
+    const cepInput = screen.getByPlaceholderText(/^cep/i);
+    const endereco1Input = screen.getByPlaceholderText(/^endereço 1/i);
+    const endereco2Input = screen.getByPlaceholderText(/^endereço 2/i);
+
+    fireEvent.change(cepInput, { target: { value: '1111' } });
+    fireEvent.change(endereco1Input, { target: { value: 'loren ipsun' } });
+    fireEvent.change(endereco2Input, { target: { value: 'loren ipsun 2' } });
+
+    await act(async () => {
+      fireEvent.click(btnNext);
+    });
+
+    expect(cepInput).toBeInTheDocument();
+    expect(endereco1Input).toBeInTheDocument();
+    expect(endereco2Input).toBeInTheDocument();
+  });
 });
