649a4bb9cda49bd6a51ef88d53237080776f400f
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-01-08 21:54:09 -0300

    feat: style theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components'
 import ListClient from './pages/ListClient';
 import NewClient from './pages/NewClient';
+import theme from './styles/theme';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<NewClient />} />
-        <Route path="/cadastros" element={<ListClient />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<NewClient />} />
+          <Route path="/cadastros" element={<ListClient />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
