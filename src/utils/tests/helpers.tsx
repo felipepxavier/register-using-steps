c54782bd441ebee1bf84c2db72ab1a4aaf9cd304
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-02-04 23:38:58 -0300

    feat: renderWithRouter helper

--- src/utils/tests/helpers.tsx
+++ src/utils/tests/helpers.tsx
@@ -1,6 +1,24 @@
-import { ThemeProvider } from 'styled-components'
-import { render, RenderResult } from '@testing-library/react'
-import theme from 'styles/theme'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from 'store';
+import { ThemeProvider } from 'styled-components';
+import { render, RenderResult } from '@testing-library/react';
+import theme from 'styles/theme';
 
 export const renderWithTheme = (children: React.ReactNode): RenderResult =>
-  render(<ThemeProvider theme={theme}>{children}</ThemeProvider>)
+  render(
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </Provider>
+  );
+
+export const renderWithThemeEndRouter = (
+  children: React.ReactNode
+): RenderResult =>
+  render(
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>{children}</BrowserRouter>
+      </ThemeProvider>
+    </Provider>
+  );
