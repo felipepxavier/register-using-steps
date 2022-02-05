a8cdd66fa3cbaaef84824f6bc1c3e8453ee1f264
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-02-05 00:05:49 -0300

    test: should navigate if click in Visualizar button

--- src/pages/ListUser/ListUser.spec.tsx
+++ src/pages/ListUser/ListUser.spec.tsx
@@ -1,5 +1,6 @@
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { renderWithThemeEndRouter } from 'utils/tests/helpers';
+import { useNavigate } from 'react-router-dom';
 
 import store from 'store';
 
@@ -48,5 +49,34 @@ describe('<ListUser/>', () => {
     expect(screen.getByText('Nome')).toBeInTheDocument();
     expect(screen.getByText('Sobrenome')).toBeInTheDocument();
     expect(screen.getByText('E-mail')).toBeInTheDocument();
+  });
+
+  it('should navigate if click in Visualizar button', () => {
+    (store.getState as jest.Mock).mockReturnValue({
+      users: [
+        {
+          id: '666',
+          name: 'mocked',
+          lastName: 'mocked',
+          email: 'mocked@gmail.com',
+          phone: '5198556666',
+          zipCode: '94080100',
+          address1: 'loren ipsun 01',
+          address2: 'loren ipsun 02',
+          birthData: '10/08/1994',
+          cpf: '666666666-18',
+          rent: '25000',
+        },
+      ],
+    });
+
+    const navigateMocked = jest.fn();
+    (useNavigate as jest.Mock).mockReturnValue(navigateMocked);
+    renderWithThemeEndRouter(<ListUser />);
+
+    const buttonView = screen.getByRole('button', { name: /visualizar/i });
+    fireEvent.click(buttonView);
+
+    expect(navigateMocked).toHaveBeenCalledTimes(1);
   });
 });
