5c5ddc54874376831d9997d2cbff08b711f2a016
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-02-04 23:40:17 -0300

    test: should render list user correctly

---
+++ src/pages/ListUser/ListUser.spec.tsx
@@ -0,0 +1,52 @@
+import { screen } from '@testing-library/react';
+import { renderWithThemeEndRouter } from 'utils/tests/helpers';
+
+import store from 'store';
+
+import ListUser from '.';
+
+jest.mock('store', () => ({
+  subscribe: jest.fn(),
+  dispatch: jest.fn(),
+  getState: jest.fn(),
+}));
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn(),
+}));
+
+describe('<ListUser/>', () => {
+  it('should render list correctly', () => {
+    (store.getState as jest.Mock).mockReturnValue({
+      users: [
+        {
+          id: '666',
+          name: 'mocked',
+          lastName: 'mocked',
+          email: 'mocked@gmail.com',
+          phone: '5198556666',
+          zipCode: '94080100',
+          address1: 'loren ipsun 01',
+          address2: 'loren ipsun 02',
+          birthData: '10/08/1994',
+          cpf: '666666666-18',
+          rent: '25000',
+        },
+      ],
+    });
+    renderWithThemeEndRouter(<ListUser />);
+
+    expect(
+      screen.getByRole('heading', { name: /listar clientes/i })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: /Lista de cadastros/i })
+    ).toBeInTheDocument();
+
+    expect(screen.getByText('Nome')).toBeInTheDocument();
+    expect(screen.getByText('Sobrenome')).toBeInTheDocument();
+    expect(screen.getByText('E-mail')).toBeInTheDocument();
+  });
+});
